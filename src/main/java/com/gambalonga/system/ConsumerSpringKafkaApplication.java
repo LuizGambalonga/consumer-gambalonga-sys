03bc85a1366fa006bcd27fbbc269096c545338e0
= <=>
2025-02-19 22:23:44 -0300

    Initial Commit

---
+++ src/main/java/com/gambalonga/system/ConsumerSpringKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.gambalonga.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerSpringKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerSpringKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gambalonga/system/ConsumerSpringKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gambalonga.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerSpringKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
