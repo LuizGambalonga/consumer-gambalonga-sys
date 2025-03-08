2ea1aff07c5cf7eae048bfeaba83ecc3f614757a
= <=>
2025-03-08 20:30:45 -0300

    melhor legibilidade, organizacao dos services e estrutura dos dados.

--- src/main/java/com/gambalonga/kafka/UsuarioConsumer.java
+++ src/main/java/com/gambalonga/kafka/UsuarioConsumer.java
@@ -1,9 +1,12 @@
 package com.gambalonga.kafka;
 
+import com.gambalonga.usuario.enums.UsuarioAtivo;
 import com.gambalonga.usuario.model.UsuarioModel;
 import com.gambalonga.usuario.repository.UsuarioRepository;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
+import static com.gambalonga.usuario.enums.UsuarioAtivo.ATIVADO;
 
 @Component
 public class UsuarioConsumer {
@@ -18,7 +21,7 @@ public class UsuarioConsumer {
     public void ativarUsuario(String email) {
         UsuarioModel usuario = usuarioRepository.findByEmail(email).orElse(null);
         if (usuario != null) {
-            usuario.setStatus("ativo");
+            usuario.setAtivo(ATIVADO);
             usuarioRepository.save(usuario);
         }
     }
